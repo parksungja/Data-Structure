1480d4d8d1dcf5cedc9b4a7bd8f82075a713b068
parksungja <167094732+parksungja@users.noreply.github.com>
2025-04-15 14:07:16 +0900

    Update 20240954_queue_report1.c

--- 7주차/20240954_queue_report1.c
+++ 7주차/20240954_queue_report1.c
@@ -4,7 +4,7 @@
 #define SIZE 30 // 큐의 크기 30으로 설정
 
 typedef struct {
-    int data[SIZE];
+    int value[SIZE];
     int front;
     int rear;
 } LinearQueue;
@@ -32,7 +32,7 @@ bool enqueue(LinearQueue *q, int value) {
         return false;
     }
     q -> rear++;
-    q -> data[(q -> rear)] = value;
+    q -> value[(q -> rear)] = value;
     return true;
 }
 
@@ -43,29 +43,41 @@ int dequeue(LinearQueue *q) {
         initQueue(q); // 큐를 초기화
         return false;
     }
-    int value = q -> data[q -> front];
+    int value = q -> value[q -> front];
     q -> front++;
     return value;
+}
+
+// 큐 데이터 출력 함수
+void printQueue(LinearQueue *q) {
+    if (isEmpty(q)) {
+        printf("큐가 비어있습니다.\n");
+        return;
+    }
+    printf("큐 데이터: ");
+    for (int i = q->front; i <= q->rear; i++) {
+        printf("%d ", q->value[i]);
+    }
+    printf("\n");
 }
 
 int main() {
     LinearQueue q;
     initQueue(&q);
+    int choice, value;
 
     while(1) {
-        int select;
         printf("1. 삽입 2. 삭제 3. 출력 4. 종료\n");
-        scanf("%d", &select);
+        printf("메뉴 선택: ");
+        scanf("%d", &choice);
 
-        int data;
-
-        switch (select){
+        switch (choice){
             case 1:
                 printf("삽입할 데이터 입력: ");
-                scanf("%d", &data);
+                scanf("%d", &value);
 
-                if (enqueue(&q, data)) {
-                    printf("삽입 성공: %d\n", data);
+                if (enqueue(&q, value)) {
+                    printf("삽입 성공: %d\n", value);
                 } else {
                     printf("삽입 실패: 큐가 가득 찼습니다.\n");
                 }
@@ -79,13 +91,7 @@ int main() {
                 break;
             case 3:
                 printf("큐 데이터 출력\n");
-                if (isEmpty(&q)) {
-                    printf("큐가 비어있습니다.\n");
-                } else {
-                    while (!isEmpty(&q)) {
-                        printf("Dequeue : %d\n", dequeue(&q));
-                    }
-                }
+                printQueue(&q);
                 break;
             case 4:
                 printf("프로그램 종료.\n");
